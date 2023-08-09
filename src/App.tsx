6ff757f33d5b323bc8fbeb4c57fa65dafe1e94a7
Thomas <79496740+KimYoonSu97@users.noreply.github.com>
2023-08-09 21:15:46 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Router from './shared/Router';
-import GlobalStyle from './globalStyle';
+import GlobalStyle from './GlobalStyle';
 const App = () => {
   return (
     <>
