8e9c209fd77e950d87448b16bd3d760ecf7ec599
TerryEHLee <terryehlee@gmail.com>
2023-08-09 21:12:06 +0900

    Feat-Join 회원가입, 회원정보기입,
    매일,grade 업데이트예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Router from './shared/Router';
-import GlobalStyle from './GlobalStyle';
+import GlobalStyle from './globalStyle';
 const App = () => {
   return (
     <>
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { supabase } from '../../supabase';
 
 const Main = () => {
   return <div>Main</div>;
