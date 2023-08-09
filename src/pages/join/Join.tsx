3215d7b3db3e225bf6a5ee2088be74eb8d4175a7
TerryEHLee <terryehlee@gmail.com>
2023-08-10 08:31:29 +0900

    Feat join 로그인페이지에서 '회원가입하러가기' 버튼 추가

--- src/pages/join/Join.tsx
+++ src/pages/join/Join.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from '../../supabase';
 import { useNavigate } from 'react-router-dom';
 
