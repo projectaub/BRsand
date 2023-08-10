f8dc2f4e14d1dabc0a8bbc55d235e1e4687ea25b
TerryEHLee <terryehlee@gmail.com>
2023-08-10 09:33:43 +0900

    Feat - join
    회원 가입시,
    개인정보 입력을 선택할 경우
    개인정보 입력 모달창이 나타나 개인정보 입력가능.
    추가로
    Grade와email값은 자동으로 업데이트 됨.

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/main/Main';
 import Join from '../pages/join/Join';
