aba9b6e34eaa6c89147bbf9a63e530c7c1de8920
KimYoonSu97 <zkzk625@naver.com>
2023-08-10 14:10:39 +0900

    Fix: 로그인 선택 기능

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,6 +6,33 @@ import LoginOTP from '../../components/login/LoginOTP';
 
 const Login = () => {
   return <div></div>;
+  const [loading, setLoading] = useState(false);
+  const [selectLogin, setSelectLogin] = useState(true);
+
+  const navigate = useNavigate();
+
+  const moveJoinPage = async () => {
+    navigate('/join');
+  };
+
+  const selectLoginWay = () => {
+    setSelectLogin(!selectLogin);
+  };
+
+  return (
+    <div>
+      {selectLogin ? <LoginBasic></LoginBasic> : <LoginOTP></LoginOTP>}
+      <div>
+        <button onClick={selectLoginWay}>
+          {selectLogin ? '이메일 인증으로 간편로그인 하기로 변경' : '아이디 패스워드로 로그인 하기로 변경'}
+        </button>
+      </div>
+
+      <div>
+        <button onClick={moveJoinPage}>회원가입하러 가기</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
