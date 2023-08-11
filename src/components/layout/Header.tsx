34abfe2067de2e04eb046fab5745cdcea2c3c379
KimYoonSu97 <zkzk625@naver.com>
2023-08-11 15:58:37 +0900

    design 로고 / 파비콘 / 상단바 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+// import   from '../../static/LogoImg.png';
 import { styled } from 'styled-components';
 
 const Header = () => {
-  return <S.Area>LOGO</S.Area>;
+  return (
+    <S.Area>
+      <S.LogoArea>
+        <S.LogoImg src="https://i.ibb.co/ZT0yLjj/02.png"></S.LogoImg>
+      </S.LogoArea>
+    </S.Area>
+  );
 };
 
 export default Header;
@@ -11,10 +18,14 @@ const S = {
   Area: styled.div`
     width: 100%;
     height: 70px;
-    background-color: #226f54;
+    background-color: #b73d52;
     display: flex;
     justify-content: center;
     align-items: center;
     color: white;
+  `,
+  LogoArea: styled.div``,
+  LogoImg: styled.img`
+    width: 130px;
   `
 };
