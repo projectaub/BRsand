f0f3e9e6b65dc53b4a73aabc93c756927b914f6a
[Mia Lee] <korea_stan@naver.com>
2023-08-11 15:47:11 +0900

    파비콘

--- src/pages/admin/login/AdminLogin.tsx
+++ src/pages/admin/login/AdminLogin.tsx
@@ -30,7 +30,8 @@ const AdminLogin = () => {
         return;
       }
 
-      const response = await supabase.from('admin').select().eq('id', adminEmail).single();
+      const response = await supabase.from('admin').select().eq('email', adminEmail).single();
+      console.log(response.data);
       if (response.data.storeId === 0) {
         navigate(`/statistics/${response.data.storeId}`);
       } else {
