41edcf84771a1b6efb6f701d4cd7d968ace85dfb
[Mia Lee] <korea_stan@naver.com>
2023-08-13 00:40:17 +0900

    메뉴랑 DB 연동 확인

--- src/pages/orderpage/OrderMenu.tsx
+++ src/pages/orderpage/OrderMenu.tsx
@@ -3,11 +3,14 @@ import useGetOrder from '../../hook/useGetOrder';
 import { supabase } from '../../supabase';
 import OrderPay from '../../components/order detail/OrderPay';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { styled } from 'styled-components';
 
 interface Menu {
   name: string;
   price: number;
   id: number;
+  img: string;
 }
 
 const OrderMenu = () => {
@@ -16,19 +19,32 @@ const OrderMenu = () => {
 
   const [orderId] = useGetOrder();
   const [pay, setPay] = useState(false);
-  const [menu, setMenu] = useState<Menu>();
+  const [menu, setMenu] = useState<Menu | undefined>();
+  const [menuList, setMenuList] = useState<Menu[]>([]);
 
   //메뉴 리스트를 서버에서 가져오면 좋을거같습니다.
   //나중에 전체 어드민에서 새로운 메뉴를 추가할 수 있도록..?
-  const menuList = [
-    { name: '바베큐치킨', price: 50000, id: 1 },
-    { name: '랜치비건', price: 40000, id: 2 },
-    { name: '마라비프', price: 100000, id: 3 },
-    { name: '스위트어니언햄', price: 8000, id: 4 },
-    { name: '핫칠리에그마요', price: 3000, id: 5 }
-  ];
+  // const menuList = [
+  //   { name: '바베큐치킨', price: 50000, id: 1 },
+  //   { name: '랜치비건', price: 40000, id: 2 },
+  //   { name: '마라비프', price: 100000, id: 3 },
+  //   { name: '스위트어니언햄', price: 8000, id: 4 },
+  //   { name: '핫칠리에그마요', price: 3000, id: 5 }
+  // ];
 
   //
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data, error } = await supabase.from('menu').select();
+    console.log(data);
+    setMenuList(data!);
+    if (error) {
+      console.error('Error fetching orders:', error);
+    }
+  };
 
   //통계 css
 
@@ -50,10 +66,10 @@ const OrderMenu = () => {
       {/* 결제모달창 컴포넌트입니다. */}
       {pay && <OrderPay updateOrder={updateOrder} setPay={setPay}></OrderPay>}
 
-      <h1>메뉴를 골라주세요.</h1>
+      <S.Caption>메뉴를 골라주세요.</S.Caption>
       {menuList?.map((menu) => {
         return (
-          <button
+          <S.MenuBtn
             key={menu.id}
             onClick={() => {
               setMenu(menu);
@@ -61,12 +77,29 @@ const OrderMenu = () => {
             }}
           >
             {menu.name}, {menu.price}
-          </button>
+            <img src={menu.img} style={{ width: '160px' }} />
+          </S.MenuBtn>
         );
       })}
-      <div>OrderMenu</div>
     </>
   );
+};
+
+const S = {
+  Caption: styled.p`
+    font-size: 18px;
+    margin: 8px 0px;
+    font-weight: 700;
+    margin-left: 15px;
+    color: #b73d52;
+  `,
+  MenuBtn: styled.p`
+    width: 180px;
+    height: 180px;
+    border: 2px solid #b73d52;
+    border-radius: 10px;
+    display: inline-table;
+  `
 };
 
 export default OrderMenu;
