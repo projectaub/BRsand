37945c7887d0a70ec567010ace17bbe8cdc923ca
TerryEHLee <terryehlee@gmail.com>
2023-08-12 16:15:03 +0900

    Feat - validationcheck adminLogin page

--- src/pages/admin/login/AdminLogin.tsx
+++ src/pages/admin/login/AdminLogin.tsx
@@ -13,13 +13,26 @@ const AdminLogin = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
+    setError
   } = useForm<FormValue>();
   const navigate = useNavigate();
 
   const adminLogin: SubmitHandler<FormValue> = async (data: FormValue) => {
     try {
       const { adminEmail, adminPw } = data;
+
+      const { data: userData, error: userError } = await supabase
+        .from('users')
+        .select('id')
+        .eq('email', adminEmail)
+        .single();
+
+      if (userError) {
+        setError('adminEmail', { type: 'manual', message: '등록되지 않은 메일' });
+        return;
+      }
+
       const { data: authData, error } = await supabase.auth.signInWithPassword({
         email: adminEmail,
         password: adminPw
@@ -27,8 +40,15 @@ const AdminLogin = () => {
       console.log(data);
 
       if (error) {
-        console.log('어드민로그인 에러', error);
-        return;
+        console.log(error);
+        const errorDescription = (error as any).error_description || error.message;
+        if (error.message.includes('Invalid login credentials')) {
+          setError('adminPw', { type: 'manual', message: '잘못된 비밀번호' });
+        } else {
+          alert(errorDescription);
+        }
+      } else {
+        alert('로그인');
       }
 
       const response = await supabase.from('admin').select().eq('email', adminEmail).single();
@@ -58,6 +78,7 @@ const AdminLogin = () => {
               />
               {errors.adminEmail && errors.adminEmail.type === 'required' && <p>메일을 입력하세요</p>}
               {errors.adminEmail && errors.adminEmail.type === 'pattern' && <p>올바른 메일 형식이 아닙니다</p>}
+              {errors.adminEmail && <p>{errors.adminEmail.message}</p>}
             </div>
             <div>
               <S.InputArea
@@ -70,6 +91,7 @@ const AdminLogin = () => {
               />
               {errors.adminPw && errors.adminPw.type === 'required' && <p>비밀번호를 입력하세요</p>}
               {errors.adminPw && errors.adminPw.type === 'minLength' && <p>비밀번호는 최소 6자리 이상</p>}
+              {errors.adminPw && <p>{errors.adminPw.message}</p>}
             </div>
             <S.SubmitButton type="submit">어드민 로그인</S.SubmitButton>
           </S.InputContainer>
